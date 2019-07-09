b36b8a42d4adcc272b266755ec2f00d208ee6004
Ronald Ngoda <34179984+ronniengoda@users.noreply.github.com>
2019-07-09 16:33:27 +0300

    Update lnmo.php

--- lnmo.php
+++ lnmo.php
@@ -2,28 +2,34 @@
 require_once 'PHPSAPGateway.php';
 $gateway= new PhpSapGateway;
 
-//Set your authentication credentials below
+//Set your authentication credentials below(Required)
 $username="username";
 $apiKey="api key";
 
-//Set PhoneNumber and Amount below
+//Set PhoneNumber and Amount below(Required)
 $PhoneNumber="+254708344101";
 $Amount="10";
+
+//Set the metadata you want to attach to the request below(optional);
+$LNMOmetadata = [
+		"MetaData"   => "0987654321"
+	];
 
 //Pass authentication credentials and your LNMO data into an array
 $LNMOData = array(
 	'PhoneNumber' => $PhoneNumber,
 	'Amount' => $Amount,
 	'username'=>$username,
-	'apiKey'=>$apiKey
+	'apiKey'=>$apiKey,
+	'LNMOmetadata'=>$LNMOmetadata
 );
 
-	//Convert the array into JSON string.
+//Convert the array into JSON string.
 $LNMODataEncoded = json_encode($LNMOData);
 
 	//Thats it,from here we will take care of the rest.
 try {
-	$result=$gateway->ReceiveLNMOData($LNMODataEncoded);
+	$result=$gateway->ProcessLNMO($LNMODataEncoded);
 } catch (Exception $e) {
 	echo $e->getMessage();
 }
