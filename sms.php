d557d17dc4b5d7f7e7d086f816185d6c16620784
Ronald Ngoda <34179984+ronniengoda@users.noreply.github.com>
2019-07-09 16:35:56 +0300

    Update sms.php

--- sms.php
+++ sms.php
@@ -2,13 +2,13 @@
 require_once 'PHPSAPGateway.php';
 $gateway= new PhpSapGateway;
 
-//Set your authentication credentials below
-$username="sanxbox";
-$apiKey="api key here";
+//Set your authentication credentials below(Required)
+$username="username";
+$apiKey="api key";
 
-//Set SMS Receiver(in international format for this case +254) and Message below
+//Set SMS Receiver(in international format for this case +254) and Message below(Required)
 $Receiver="+254708344101";
-$Message="Hello there,PHP SAP Here!!";
+$Message="i love nerds";
 
 //Pass authentication credentials and your SMS data into an array
 $SMSData = array(
@@ -23,7 +23,7 @@ $SMSDataEncoded = json_encode($SMSData);
 
 	//Thats it,from here we will take care of the rest.
 try {
-	$result=$gateway->ReceiveSMSData($SMSDataEncoded);
+	$result=$gateway->ProcessSMS($SMSDataEncoded);
 } catch (Exception $e) {
 	echo $e->getMessage();
 }
