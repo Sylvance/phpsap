3b266c84e7112dc969e7f0b09070571e03788e91
Ronald Ngoda <34179984+ronniengoda@users.noreply.github.com>
2019-07-12 18:24:50 +0300

    Update payments_wallet_transfer.php

--- payments_wallet_transfer.php
+++ payments_wallet_transfer.php
@@ -3,11 +3,11 @@ require_once 'PHPSAPGateway.php';
 $gateway= new PhpSapGateway;
 
 //Set your authentication credentials below(Required)
-$username="ronniengoda";
-$apiKey="z3qww3f7z27b19h2chumz1cs2uhrt8pxo574sphh1szbm59fx0poufadwgbnf9fu";
+$username="username";
+$apiKey="api key";
 
 // Set the DestinationAccountName and Amount(Required)
-$DestinationAccountName ="ronniengoda";
+$DestinationAccountName ="accountname";
 $Amount="10";
 
 //Pass authentication credentials and your Transfer data into an array
