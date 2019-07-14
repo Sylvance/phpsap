2381a143a662b8993debca1db22ce5ba49c6564e
Ronald Ngoda <34179984+ronniengoda@users.noreply.github.com>
2019-07-14 16:59:06 +0300

    Update sms.php

--- sms.php
+++ sms.php
@@ -24,6 +24,7 @@ $SMSDataEncoded = json_encode($SMSData);
 	//Thats it,from here we will take care of the rest.
 try {
 	$result=$gateway->ProcessSMS($SMSDataEncoded);
+	print_r($result);
 } catch (Exception $e) {
 	echo $e->getMessage();
 }
