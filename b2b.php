f339d56060a62106dcd2c389e50c70a7d018117f
Ronald Ngoda <34179984+ronniengoda@users.noreply.github.com>
2019-07-14 16:51:29 +0300

    Update b2b.php

--- b2b.php
+++ b2b.php
@@ -7,8 +7,8 @@ $username="username";
 $apiKey="api key";
 
 // Set the destination channel,destination account and amount(Required)
-$DestinationChannel ="paybill no";
-$DestinationAccount ="account";
+$DestinationChannel ="525900";
+$DestinationAccount ="renthero.api";
 $Amount="10";
 
 //Pass authentication credentials and your B2B data into an array
@@ -26,6 +26,7 @@ $B2BDataEncoded = json_encode($B2BData);
 	//Thats it,from here we will take care of the rest.
 try {
 	$result=$gateway->ProcessB2B($B2BDataEncoded);
+	print_r($result);
 } catch (Exception $e) {
 	echo $e->getMessage();
 }
