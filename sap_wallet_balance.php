1606dc7ed2f57ea97f4fe51e38562ad9e6112c3b
Ronald Ngoda <34179984+ronniengoda@users.noreply.github.com>
2019-07-14 16:58:11 +0300

    Update sap_wallet_balance.php

--- sap_wallet_balance.php
+++ sap_wallet_balance.php
@@ -4,7 +4,7 @@ $gateway= new PhpSapGateway;
 
 //Set your authentication credentials below(Required)
 $username="username";
-$apiKey="pi key";
+$apiKey="api key";
 
 //Pass authentication into an array
 $SAPWalletBalanceData = array(
@@ -18,6 +18,7 @@ $SAPWalletBalanceDataEncoded = json_encode($SAPWalletBalanceData);
 //Thats it,from here we will take care of the rest.
 try {
 	$result=$gateway->ProcessSAPWalletBalance($SAPWalletBalanceDataEncoded);
+	print_r($result);
 } catch (Exception $e) {
 	echo $e->getMessage();
 }
