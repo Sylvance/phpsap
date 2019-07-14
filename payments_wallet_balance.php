ed958634178b90f58e891172b0276b1bbafeaed5
Ronald Ngoda <34179984+ronniengoda@users.noreply.github.com>
2019-07-14 16:55:15 +0300

    Update payments_wallet_balance.php

--- payments_wallet_balance.php
+++ payments_wallet_balance.php
@@ -18,6 +18,7 @@ $PaymentsWalletBalanceDataEncoded = json_encode($PaymentsWalletBalanceData);
 //Thats it,from here we will take care of the rest.
 try {
 	$result=$gateway->ProcessBalance($PaymentsWalletBalanceDataEncoded);
+	print_r($result);
 } catch (Exception $e) {
 	echo $e->getMessage();
 }
